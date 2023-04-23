de644a1bf32c4e174ca229a165854ecfe5d2bc76
Maksym Bratus <mbmaksbrat12345@gmail.com>
2023-04-23 17:37:28 +0300

    added adaptivity

--- js/script.js
+++ js/script.js
@@ -1,209 +1,152 @@
-$(document).ready(function(){
-    $(".slider_header").slick({
-        arrows: true,
-        dots: true,
-        adaptiveHeight: true,
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        speed: 1500,
-        easing: 'easy', 
-        infinite: true, 
-        initialSlide: 0, // start slide
-        autoplay: true,
-        autoplaySpeed: 3500,
-        pauseOnFocus: true,
-        pauseOnHover: true,
-        pauseOnDots: true,
-        draggable: false, //move with mouse only on PC
-        swipe: false, //move only on mouse
-        touchThreshold: 10, //how much u need to scroll
-        touchMove: false, // we cannot to drag
-        waitForAnimate: true,
-        vertical: true,
-    });
+$(".slider_header").slick({
+    arrows: true,
+    dots: true,
+    adaptiveHeight: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 1500,
+    easing: 'easy', 
+    infinite: true, 
+    initialSlide: 0, 
+    autoplay: false,
+    autoplaySpeed: 3500,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDots: true,
+    draggable: false, 
+    swipe: false,
+    touchThreshold: 10,
+    touchMove: false,
+    waitForAnimate: true,
+    vertical: true,
 });
 
-$(document).ready(function(){
-    $(".popular__slider").slick({
-        arrows: true,
-        dots: false,
-        adaptiveHeight: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        speed: 1500,
-        easing: 'easy', 
-        infinite: false, 
-        initialSlide: 1, // start slide
-        autoplay: false,
-        pauseOnFocus: true,
-        pauseOnHover: true,
-        pauseOnDots: true,
-        draggable: false, //move with mouse only on PC
-        swipe: false, //move only on mouse
-        touchThreshold: 10, //how much u need to scroll
-        touchMove: false, // we cannot to drag
-        waitForAnimate: true,
-        variableWidth: true,
-        responsive: [
-            {
-              breakpoint: 1440,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-              }
+
+$(".popular__slider").slick({
+    arrows: true,
+    dots: false,
+    adaptiveHeight: true,
+    slidesToScroll: 1,
+    speed: 1500,
+    easing: 'easy', 
+    infinite: false, 
+    initialSlide: 1, // start slide
+    autoplay: false,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDots: true,
+    draggable: false,
+    swipe: false, 
+    touchThreshold: 10,
+    touchMove: false,
+    waitForAnimate: true,
+    variableWidth: true,
+    responsive: [
+        {
+            breakpoint: 780,
+            settings: {
+            slidesToShow: 1,
+            variableWidth: false,
+            centerMode: false,
             }
-        ]
-    });
+        },
+    ]
 });
 
-
-$(document).ready(function(){
-    $(".special__slider").slick({
-        arrows: true,
-        dots: false,
-        adaptiveHeight: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        speed: 1500,
-        easing: 'easy', 
-        infinite: true, 
-        initialSlide: 1, // start slide
-        autoplay: true,
-        pauseOnFocus: true,
-        pauseOnHover: true,
-        pauseOnDots: true,
-        draggable: false, //move with mouse only on PC
-        swipe: false, //move only on mouse
-        touchThreshold: 10, //how much u need to scroll
-        touchMove: false, // we cannot to drag
-        waitForAnimate: true,
-        centerMode: true,
-        variableWidth: true,
-    });
-});
-
-$(document).ready(function(){
-    $(".popular__slider").slick({
-        arrows: true,
-        dots: false,
-        adaptiveHeight: true,
-        slidesToShow: 3,
-        slidesToScroll: 1,
-        speed: 1500,
-        easing: 'easy', 
-        infinite: false, 
-        initialSlide: 1, // start slide
-        autoplay: false,
-        pauseOnFocus: true,
-        pauseOnHover: true,
-        pauseOnDots: true,
-        draggable: false, //move with mouse only on PC
-        swipe: false, //move only on mouse
-        touchThreshold: 10, //how much u need to scroll
-        touchMove: false, // we cannot to drag
-        waitForAnimate: true,
-        variableWidth: true,
-        responsive: [
-            {
-              breakpoint: 1440,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-              }
-            },
-        ]
-    });
-});
-
-$(document).ready(function(){
-    $(".trip-planners__slider").slick({
-        arrows: false,
-        dots: false,
-        adaptiveHeight: true,
-        slidesToShow: 4,
-        slidesToScroll: 0,
-        speed: 1500,
-        easing: 'easy', 
-        infinite: false, 
-        initialSlide: 0, // start slide
-        autoplay: false,
-        pauseOnFocus: true,
-        pauseOnHover: true,
-        pauseOnDots: true,
-        draggable: true, //move with mouse only on PC
-        swipe: true, //move only on mouse
-        touchMove: true, // we cannot to drag
-        waitForAnimate: true,
-        centerMode: true,
-        variableWidth: true,
-        responsive: [
-            {
-              breakpoint: 1440,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-              }
+$(".special__slider").slick({
+    arrows: true,
+    dots: false,
+    adaptiveHeight: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 1500,
+    easing: 'easy', 
+    infinite: true, 
+    initialSlide: 1,
+    autoplay: false,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDots: true,
+    draggable: false,
+    swipe: false,
+    touchThreshold: 10, 
+    touchMove: false,
+    waitForAnimate: true,
+    centerMode: true,
+    variableWidth: true,
+    responsive: [
+        {
+            breakpoint: 780,
+            settings: {
+            slidesToShow: 1,
+            variableWidth: false,
+            centerMode: false,
             }
-        ]
-    });
+        },
+    ]
 });
 
-$(document).ready(function(){
-    $(".gallery__slider").slick({
-        arrows: true,
-        dots: false,
-        adaptiveHeight: true,
-        slidesToShow: 4,
-        slidesToScroll: 1,
-        speed: 1500,
-        easing: 'easy', 
-        infinite: true, 
-        initialSlide: 1, // start slide
-        autoplay: false,
-        pauseOnFocus: false,
-        pauseOnHover: false,
-        pauseOnDots: false,
-        draggable: true, //move with mouse only on PC
-        swipe: true, //move only on mouse
-        touchThreshold: 10, //how much u need to scroll
-        touchMove: true, // we cannot to drag
-        waitForAnimate: true,
-        centerMode: false,
-        variableWidth: true,
-    });
-});
-
-
-$(document).ready(function(){
-    $(".travelers__slider").slick({
-        arrows: true,
-        dots: false,
-        adaptiveHeight: true,
-        slidesToShow: 2,
-        slidesToScroll: 1,
-        speed: 1500,
-        easing: 'easy', 
-        infinite: false, 
-        initialSlide: 0, // start slide
-        autoplay: false,
-        pauseOnFocus: true,
-        pauseOnHover: true,
-        pauseOnDots: true,
-        draggable: false, //move with mouse only on PC
-        swipe: false, //move only on mouse
-        touchThreshold: 10, //how much u need to scroll
-        touchMove: false, // we cannot to drag
-        waitForAnimate: true,
-        centerMode: false,
-        variableWidth: true,
-        responsive: [
-            {
-              breakpoint: 1440,
-              settings: {
-                slidesToShow: 2,
-                slidesToScroll: 1,
-              }
+$(".gallery__slider").slick({
+    arrows: true,
+    dots: false,
+    adaptiveHeight: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    speed: 1500,
+    easing: 'easy', 
+    infinite: true, 
+    initialSlide: 1,
+    autoplay: false,
+    pauseOnFocus: false,
+    pauseOnHover: false,
+    pauseOnDots: false,
+    draggable: false,
+    swipe: false, 
+    touchThreshold: 10,
+    touchMove: true,
+    waitForAnimate: true,
+    centerMode: false,
+    variableWidth: true,
+    responsive: [
+        {
+            breakpoint: 780,
+            settings: {
+            rows: 4,
+            slidesToShow: 1,
+            variableWidth: false,
             }
-        ]
-    });
+        }
+    ]
+});
+
+$(".travelers__slider").slick({
+    arrows: true,
+    dots: false,
+    adaptiveHeight: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    speed: 1500,
+    easing: 'easy', 
+    infinite: true, 
+    initialSlide: 0,
+    autoplay: false,
+    pauseOnFocus: true,
+    pauseOnHover: true,
+    pauseOnDots: true,
+    draggable: false, 
+    swipe: false,
+    touchThreshold: 10, 
+    touchMove: false,
+    waitForAnimate: true,
+    variableWidth: true,
+    responsive: [
+        {
+            breakpoint: 780,
+            settings: {
+            slidesToShow: 1,
+            variableWidth: false,
+            centerMode: false,
+            }
+        },
+    ]
 });
